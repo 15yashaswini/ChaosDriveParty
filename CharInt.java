6a3863d91a7de12d46f1e34b8006b8cc1313e5d2
15yashaswini <24yashaswini@gmail.com>
2023-07-02 16:10:21 +0530

    FirstGitPush
    Some files to test

---
+++ CharInt.java
@@ -0,0 +1,12 @@
+class CharInt 
+{
+	public static void main(String[] args) 
+	{
+		char c ='Y';
+		int a;
+		a=c;
+		System.out.println(c);
+		System.out.println(a);
+
+	}
+}
---
+++ CharLong.java
@@ -0,0 +1,11 @@
+class CharLong
+{
+	public static void main(String[] args) 
+	{
+		char c = 'Y';
+		long a;
+		a=c;
+		System.out.println(c);
+		System.out.println(a);
+	}
+}
